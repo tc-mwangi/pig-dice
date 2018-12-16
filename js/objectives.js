d451e804d5731202f4567a3cd26179ff33e70206
Loise Mwangi <wanjirumwangi.learn@gmail.com>
2018-12-16 21:28:54 +0300

    generate random number alert

--- js/objectives.js
+++ js/objectives.js
@@ -37,23 +37,52 @@ $(document).ready(function(){
     $("#player-two")[0].src = avatarArray2[this.value];
   })
 
-});
-
-
 
 //IP Objectives-------------------------
 //1. Player One can role dice...
+//* When Roll Dice btn is clicked, a random number is displayed on Player's score.
+//<div class="mb-20 mb-md-10 align-right">
+//   <input type="number" name="number" id="scoreCounterOne" class="input-md form-control" placeholder="Score" max="100">
+// </div>
+//
+// <div>
+//   <a id="display1" href="" onclick="rollDice()" class="btn btn-mod btn-w btn-medium btn-round ">Roll Dice</a>
+// </div>
+
+// rollDice() {
+//   var displayPoints = document.getElementById("display1").
+// }
+
+
 //* When Roll Dice is clicked, output image is displayed on the console.
 
 //<img src='' id="show-dice"  alt="dice image">
-//<a id="roll" href="" onclick="rollDice()" class="btn btn-mod btn-w btn-medium btn-round ">Roll Dice</a>
+//<a id="roll-button" href="" onclick="rollDice()" class="btn btn-mod btn-w btn-medium btn-round ">Roll Dice</a>
 
-// rollDice() {
-//   var diceArray = ["1.png","2.png","3.png","4.png","5.png","6.png"];
-//   path = path || "images/die/";
-//   //shuffle function
-//   var shuffleFace = Math.floor(Math.random() * diceArray.length);
-//   var face = diceArray[shuffleFace];
-//   var faceDisplay = "<img src=' + path + face + ' alt="dice image">    ";
-//   document.append(faceDisplay); document.close();
+  //Array of dice images 1-6
+  var diceArray = [
+  "1.png",
+  "2.png",
+  "3.png",
+  "4.png",
+  "5.png",
+  "6.png"];
+
+  //generate random number- Math.floor()  - Pass Math.random as an argument.
+  var randomNumber = Math.floor(Math.random() * (diceArray.length)) + 1;
+  alert("You rolled "+randomNumber);
+
+  
+
+
+//   function rollDice() {
+//     //Select Button
+//     $("#roll-button").click(function(){
+//
+//     })
+//
 // };
+
+
+
+}); //------ closing jquery
