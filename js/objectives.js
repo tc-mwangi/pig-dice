d408d4dbc2d45ef17ac147d8d770a685bcfa5a4a
Loise Mwangi <wanjirumwangi.learn@gmail.com>
2018-12-17 06:51:08 +0300

    edit display dice image and result function

--- js/objectives.js
+++ js/objectives.js
@@ -41,48 +41,70 @@ $(document).ready(function(){
 //IP Objectives-------------------------
 //1. Player One can role dice...
 //* When Roll Dice btn is clicked, a random number is displayed on Player's score.
-//<div class="mb-20 mb-md-10 align-right">
-//   <input type="number" name="number" id="scoreCounterOne" class="input-md form-control" placeholder="Score" max="100">
-// </div>
-//
-// <div>
-//   <a id="display1" href="" onclick="rollDice()" class="btn btn-mod btn-w btn-medium btn-round ">Roll Dice</a>
-// </div>
-
-// rollDice() {
-//   var displayPoints = document.getElementById("display1").
-// }
-
 
 //* When Roll Dice is clicked, output image is displayed on the console.
-
-//<img src='' id="show-dice"  alt="dice image">
-//<a id="roll-button" href="" onclick="rollDice()" class="btn btn-mod btn-w btn-medium btn-round ">Roll Dice</a>
+// <img id="die-display" src="images/die/1.png" alt="dice image">
+// <a id="roll-button" class="btn btn-mod btn-w btn-medium btn-round ">Roll Dice</a>
+//<p id="consoleMessage"></p>
 
   //Array of dice images 1-6
   var diceArray = [
-  "1.png",
-  "2.png",
-  "3.png",
-  "4.png",
-  "5.png",
-  "6.png"];
+  "images/die/1.png", //diceArray[0]
+  "images/die/2.png", //diceArray[1]
+  "images/die/3.png", //diceArray[2]
+  "images/die/4.png", //diceArray[3]
+  "images/die/5.png", //diceArray[4]
+  "images/die/6.png"]; //diceArray[5]
 
-  //generate random number- Math.floor()  - Pass Math.random as an argument.
-  var randomNumber = Math.floor(Math.random() * (diceArray.length)) + 1;
-  alert("You rolled "+randomNumber);
+  $("#roll-button").click(function(){
+    var randomNumber = Math.floor(Math.random() * (diceArray.length)) + 1;
+    console.log(randomNumber);//test for selector
 
-  
+    if (randomNumber === 1) {
+      $("#die-display").src = diceArray[0];
+    } else if (randomNumber === 2) {
+      $("#die-display").src = diceArray[1];
+    } else if (randomNumber === 3) {
+      $("#die-display").src = diceArray[2];
+    } else if (randomNumber === 4) {
+      $("#die-display").src = diceArray[3];
+    } else if (randomNumber === 5) {
+      $("#die-display").src = diceArray[4];
+    } else {
+      $("#die-display").src = diceArray[5];
+    }
+  });  //------ closing image counter
+
+});  //------ closing jquery loading
 
 
-//   function rollDice() {
-//     //Select Button
-//     $("#roll-button").click(function(){
-//
-//     })
-//
-// };
+  // $("#roll-button").addEventListener("click", function(){
+  //   rollDice();
+  // });
+  //
+  // $("#roll-button").change(function(){
+  //   $("#die-display").src = diceArray[this.value];
+  // })
 
 
 
-}); //------ closing jquery
+  // function rollDice() {
+  // //   //generate random number- Math.floor()  - Pass Math.random as an argument.
+  // //
+
+
+
+  // document.getElementById("scoreCounterOne").innerHTML = randomNumber
+  // alert("You rolled "+randomNumber);
+  //function to change dice number and image display
+  // function rollDice(){
+  //   $("#roll-button").click(function(){
+  //     $("#changeAvatar2").change(function(){
+  //       $("#die-display")[0].src = diceArray[this.value];
+  //     })
+  //
+  //
+  //
+  //   })
+  //
+  // }
